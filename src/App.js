6c085d7284320fbbc10154927766591b79f1b609
MahnoorIsrar <mahnoorisrar26@yahoo.com>
2020-06-08 12:39:58 -0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Class from './class.js';
+
+function App() {
+return (
+    <div>
+        <Class SubjectName="React" Timing="10"/>
+
+        <hr/>
+        <Class SubjectName="Data Structure" Timing="02"/>
+        
+    </div>
+)
+}
+
+export default App;
---
+++ src/class.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Class(probs){
+    return(
+        <div>
+        <h1>Subject of my undergrad class is {probs.SubjectName}</h1>
+        <h2>Timing of my first class is {probs.Timing}</h2>
+        
+        </div>
+    )
+
+}
+export default Class;
